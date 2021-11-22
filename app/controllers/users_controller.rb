81f985d2366a62adeb1663321b1023871873fb24
Phing An Hsiao <phinganhsiao@gmail.com>
2021-11-22 00:36:32 -0500

    user table migrtated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,5 +18,10 @@ class UsersController < ApplicationController
         end
     end
 
-    
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+
 end
